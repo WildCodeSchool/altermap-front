35237be15b61ad90ce2d198b7733ea3db9247f10
Rémy <Remypottierpro@gmail.com>
2019-12-04 13:37:01 +0100

    Add icon info

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,8 +4,8 @@ import './NavBar.css';
 function NavBar() {
   return (
     <div className="NavBar">
-      <a href="#" className="NavBar__info">info</a>
-      
+      <a href="#" className="NavBar__info">
+      <i className="fas fa-info"></i></a>
     </div>
   );
 }
