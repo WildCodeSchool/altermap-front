df9f7396aaa85181f3d7a9861b5980970d8bb668
Rémy <Remypottierpro@gmail.com>
2019-12-04 15:23:05 +0100

    Add normalize, refactor App.js and App.css and also index.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Mapper from './components/Mapper/Mapper';
 
 function App() {
-  const [coords, setCoords] = useState({
-    location: {
-      lat: 42.6976,
-      lng: 2.8954,
-    },
-    haveUserLocation: false,
-    zoom: 8,
-  });
-
-  const position = [coords.location.lat, coords.location.lng];
-
   return (
     <div className="App">
-      <Mapper position={position} zoom={coords.zoom} />
+      <Mapper position={[42.6976, 2.8954]} zoom={8} />
     </div>
   );
 }
--- src/components/Mapper/Mapper.js
+++ src/components/Mapper/Mapper.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import { Map, TileLayer} from 'react-leaflet';
-import L from 'leaflet';
+import React from 'react';
+import { Map, TileLayer } from 'react-leaflet';
+// import L from 'leaflet';
 import './Mapper.css';
 
-function Mapper ({position, zoom}){
-  return(
-    <Map className="map" center={position} zoom={zoom}>
+function Mapper({ position, zoom }) {
+  return (
+    <Map className="Mapper" center={position} zoom={zoom}>
       <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
     </Map>
