aae55d71ab7b67f99b007a429e135f68caeddaf4
sergio Prieto <saprieto5@gmail.com>
2019-12-04 13:33:22 +0100

    Add basic navigaiton bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import "./App.css";
-import Mapper from "./components/Mapper/Mapper";
-import Header from "./components/Header/Header";
+import React from 'react';
+import './App.css';
+import Mapper from './components/Mapper/Mapper';
+import Header from './components/Header/Header';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Mapper position={[42.6976, 2.8954]} zoom={8} />
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <a href="#" className="NavBar__info">info</a>
+      
+    </div>
+  );
+}
+
+export default NavBar;
