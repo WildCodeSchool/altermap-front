829dda8200c0f8ef6dfb19a8bde218787926f24b
Rémy <Remypottierpro@gmail.com>
2019-12-05 09:59:03 +0100

    Refactor for merging

--- src/components/Mapper/Mapper.js
+++ src/components/Mapper/Mapper.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import { Map, TileLayer, ZoomControl } from 'react-leaflet';
 import { BoxZoomControl } from 'react-leaflet-box-zoom';
@@ -12,7 +11,7 @@ function Mapper({ position, zoom }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <ZoomControl position="topright" className="controls" />
+      <ZoomControl position="topright" />
 
       <BoxZoomControl
         position="topright"
