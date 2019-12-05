7b70d1f9be82025d07f24c9c6714017034b683f5
Rémy <Remypottierpro@gmail.com>
2019-12-05 16:21:29 +0100

    Add control bar on right and border buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import Mapper from './components/Mapper/Mapper';
-import Header from './components/Header/Header';
-import NavBar from './components/NavBar/NavBar';
-import Info from './components/Info/Info';
-import ConstructionSiteForm from './components/ConstructionSiteForm/ConstructionSiteForm';
+import React, { useState } from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Mapper from "./components/Mapper/Mapper";
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
+import Info from "./components/Info/Info";
+import ConstructionSiteForm from "./components/ConstructionSiteForm/ConstructionSiteForm";
 
 function App() {
   const [isInfoOpen, setIsInfoOpen] = useState(false);
@@ -17,9 +17,7 @@ function App() {
       {/* Fixed elements */}
       <Header />
       <Mapper position={[42.6976, 2.8954]} zoom={8} close={closeForm} />
-      {isFormOpen
-        ? <ConstructionSiteForm close={closeForm} />
-        : ''}
+      {isFormOpen ? <ConstructionSiteForm close={closeForm} /> : ""}
       <NavBar close={closeInfo} info={isInfoOpen} />
       {/* Elements to implement into Router */}
       <Switch>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import './NavBar.css';
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faInfo } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./NavBar.css";
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import { faInfo } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function NavBar({ close, info }) {
   return (
     <div className="NavBar">
-      <Link to={info ? '/' : '/info'} className="NavBar__info" onClick={() => close()}>
-        <Icon icon={faInfo} className="NavBar__icon" />
+      <Link
+        to={info ? "/" : "/info"}
+        className="NavBar__info"
+        onClick={() => close()}
+      >
+        <a href="/" className="NavBar__info">
+          <Icon icon={faInfo} />
+        </a>
       </Link>
     </div>
   );
