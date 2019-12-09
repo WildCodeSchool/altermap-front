eddf82bf1a81baef02071bf784ee4bb3ed2954ae
Rémy <Remypottierpro@gmail.com>
2019-12-09 13:52:37 +0100

    Add localstorage of polygons

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,15 +7,9 @@ import { Link } from 'react-router-dom';
 function NavBar({ info, setInfo }) {
   return (
     <div className="NavBar">
-<<<<<<< HEAD
       <Link to={info ? '/' : '/info'} className="NavBar__info" onClick={() => setInfo(!info)}>
         <Icon icon={faInfo} className="NavBar__icon" />
       </Link>
-=======
-      <a href="/" className="NavBar__info">
-        <Icon icon={faInfo} />
-      </a>
->>>>>>> Add control bar on right and border buttons
     </div>
   );
 }
