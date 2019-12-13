e10ef9da16bd6be095800fa1be75b554aaa34f5a
TanguyDuponchelle <tanguy.duponchelle@hotmail.fr>
2019-12-13 14:13:42 +0100

    WIP Build Get method, need to set up post Method

--- src/components/Mapper/Mapper.js
+++ src/components/Mapper/Mapper.js
@@ -7,33 +7,41 @@ import L from "leaflet";
 import "./Mapper.css";
 import ConstructionSiteForm from "../ConstructionSiteForm/ConstructionSiteForm";
 
-function Mapper({ position, zoom, close, setTempCoords }) {
+function Mapper({ position, zoom, close, setTempCoords, tempCoords }) {
+  // Hook des données des polygons
   const [constructionSites, setConstructionSites] = useState(null);
+
+  // UseEffect similaire à componentDidMount
   useEffect(() => {
     axios
       .get("/api/v1/construction-sites")
       .then(response => setConstructionSites(response.data));
   }, []);
 
+  // Fonction pour afficher le GeoJson
   const getGeoJson = () => ({
     type: "FeatureCollection",
     features: [
       {
         type: "FeatureCollection",
-        features: constructionSites.map((polygon, index) => ({
-          type: "Feature",
-          properties: {
-            id: polygon.id
-          },
-          geometry: {
-            type: "Polygon",
-            coordinates: [polygon.coords]
-          }
-        }))
+        features: constructionSites.map((polygon, index) => {
+          console.log(polygon);
+          return {
+            type: "Feature",
+            properties: {
+              id: polygon.id
+            },
+            geometry: {
+              type: "Polygon",
+              coordinates: [polygon.coords]
+            }
+          };
+        })
       }
     ]
   });
 
+  // Fonction à l'éxécution du Feature Group (Container des contrôles de draw)
   const featureGroupReady = reactFGref => {
     const leafletGeoJSON = new L.GeoJSON(getGeoJson());
     const leafletFG = reactFGref.leafletElement;
@@ -48,23 +56,31 @@ function Mapper({ position, zoom, close, setTempCoords }) {
       center={position}
       zoom={zoom}
       zoomControl={false}
-      maxZoom={17}
-      minZoom={1}
+      maxZoom={17} // Set du zoom max
+      minZoom={6} // Set du zoom min
     >
+      {/* Fond de carte */}
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {console.log(constructionSites && constructionSites.map(x => x.coords))}
 
       <ZoomControl position="topright" />
 
-      <BoxZoomControl position="topright" sticky />
+      <BoxZoomControl position="topright" />
+
+      {/* Feature Group qui rassemble les controles de draw */}
       <FeatureGroup
-        ref={constructionSites && (reactFGref => featureGroupReady(reactFGref))}
+        ref={
+          constructionSites &&
+          tempCoords &&
+          (reactFGref => featureGroupReady(reactFGref))
+        }
       >
         <EditControl
           position="topright"
+          // Edition des polygons
           onEdited={e => {
             console.log(e);
           }}
+          // Création des polygons
           onCreated={e => {
             console.log(e);
             const coords = e.layer.editing.latlngs[0][0].map(x => [
@@ -72,8 +88,8 @@ function Mapper({ position, zoom, close, setTempCoords }) {
               x.lat
             ]);
             setTempCoords(coords);
-            return close();
           }}
+          // Delétion des polygons
           onDeleted={e => {
             const polygonsDelete = Object.keys(e.layers._layers);
             console.log(e);
