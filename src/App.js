5c2b9aafb896b69c040461fcd84a17842949a474
Rémy <Remypottierpro@gmail.com>
2020-01-13 16:16:05 +0100

    Add auth and redirection if not connected

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import axios from 'axios';
 import Mapper from './components/Mapper/Mapper';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
@@ -13,13 +14,22 @@ function App() {
   const [isInfoOpen, setIsInfoOpen] = useState(false);
   const [isFormOpen, setIsFormOpen] = useState(false);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isAuth, setIsAuth] = useState(localStorage.getItem('altermap-token'));
   const [shouldDisplayLayer, setShouldDisplayLayer] = useState(false);
   const closeForm = () => setIsFormOpen(!isFormOpen);
   const closeInfo = () => setIsInfoOpen(!isInfoOpen);
   const layerStatus = () => setShouldDisplayLayer(!shouldDisplayLayer);
 
+  if (isAuth) {
+    const token = localStorage.getItem('altermap-token');
+    axios.defaults.headers.common = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
   return (
     <div className="App">
+      {!isAuth && <Redirect to="/login" />}
       <Switch>
         <Route exact path="/">
           <Header />
@@ -38,7 +48,7 @@ function App() {
           {isInfoOpen && (<Info close={closeInfo} />)}
         </Route>
         <Route path="/login">
-          <Login />
+          <Login setIsAuth={setIsAuth} />
         </Route>
       </Switch>
     </div>
