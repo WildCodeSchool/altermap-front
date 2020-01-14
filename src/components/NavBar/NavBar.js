70e15831f61b53611cb9062bf8b00013b693379c
Rémy <Remypottierpro@gmail.com>
2020-01-14 10:07:58 +0100

    Refactor to leave 'test mode'

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,13 +3,10 @@ import './NavBar.css';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faInfo } from '@fortawesome/free-solid-svg-icons';
 
-function NavBar({ close, disconnect }) {
+function NavBar({ close }) {
   return (
     <div className="NavBar">
       <button type="button" className="NavBar__info" onClick={close}>
-        <Icon icon={faInfo} className="NavBar__icon" />
-      </button>
-      <button type="button" className="NavBar__info" onClick={disconnect}>
         <Icon icon={faInfo} className="NavBar__icon" />
       </button>
     </div>
