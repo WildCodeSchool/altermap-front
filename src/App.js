45d4fdc81d3d9a3d8271b313a2586bad0b5af05d
Rémy <Remypottierpro@gmail.com>
2020-01-14 09:53:58 +0100

    Auth work now

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   const [isVisible, setIsVisible] = useState(false);
   const [isFormOpen, setIsFormOpen] = useState(false);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [isAuth, setIsAuth] = useState(localStorage.getItem('altermap-token'));
+  const [isAuth, setIsAuth] = useState(localStorage['altermap-token']);
   const [shouldDisplayLayer, setShouldDisplayLayer] = useState(false);
   const closeForm = () => setIsFormOpen(!isFormOpen);
   const closeInfo = () => {
@@ -30,8 +30,12 @@ function App() {
     }
   };
   const layerStatus = () => setShouldDisplayLayer(!shouldDisplayLayer);
+  const disconnect = () => {
+    localStorage.removeItem('altermap-token');
+    setIsAuth(false);
+  };
 
-  if (isAuth) {
+  if (localStorage['altermap-token'] || isAuth) {
     const token = localStorage.getItem('altermap-token');
     axios.defaults.headers.common = {
       Authorization: `Bearer ${token}`,
@@ -40,24 +44,27 @@ function App() {
 
   return (
     <div className="App">
-      {!isAuth && <Redirect to="/login" />}
+      {!localStorage['altermap-token'] && <Redirect to="/login" />}
       <Switch>
-        <Route exact path="/">
-          <Header />
-          <Mapper
-            position={[42.6976, 2.8954]}
-            zoom={8}
-            close={closeForm}
-            popup={isPopupOpen}
-            setPopupStatus={setIsPopupOpen}
-            displayLayer={shouldDisplayLayer}
-          />
-          <NavBar close={closeInfo} info={isInfoOpen} />
-          <Layers displayLayer={layerStatus} />
-          {isFormOpen && <ConstructionSiteForm close={closeForm} visible={isVisible} />}
-          {/* Elements to implement into Router */}
-          {isInfoOpen && (<Info close={closeInfo} visible={isVisible} />)}
-        </Route>
+        { isAuth
+          && (
+          <Route exact path="/">
+            <Header />
+            <Mapper
+              position={[42.6976, 2.8954]}
+              zoom={8}
+              close={closeForm}
+              popup={isPopupOpen}
+              setPopupStatus={setIsPopupOpen}
+              displayLayer={shouldDisplayLayer}
+            />
+            <NavBar close={closeInfo} disconnect={disconnect} />
+            <Layers displayLayer={layerStatus} />
+              {isFormOpen && <ConstructionSiteForm close={closeForm} />}
+              {/* Elements to implement into Router */}
+              {isInfoOpen && (<Info close={closeInfo} />)}
+          </Route>
+          )}
         <Route path="/login">
           <Login setIsAuth={setIsAuth} />
         </Route>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,10 +3,13 @@ import './NavBar.css';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faInfo } from '@fortawesome/free-solid-svg-icons';
 
-function NavBar({ close, info, displayLayer }) {
+function NavBar({ close, disconnect }) {
   return (
     <div className="NavBar">
       <button type="button" className="NavBar__info" onClick={close}>
+        <Icon icon={faInfo} className="NavBar__icon" />
+      </button>
+      <button type="button" className="NavBar__info" onClick={disconnect}>
         <Icon icon={faInfo} className="NavBar__icon" />
       </button>
     </div>
