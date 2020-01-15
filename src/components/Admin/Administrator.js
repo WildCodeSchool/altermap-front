c37d27108982aece1edfbe98db253ea5393d9bf7
Rémy <Remypottierpro@gmail.com>
2020-01-15 15:35:18 +0100

    Add authentication for react admin

--- src/components/Admin/Administrator.js
+++ src/components/Admin/Administrator.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { fetchUtils, Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import simpleRestProvider from 'ra-data-simple-rest';
 
 import { UserList, UserEdit, UserCreate } from '../Users/Users';
 
 function Administrator() {
+  const httpClient = (url, options = {}) => {
+    if (!options.headers) {
+      options.headers = new Headers({ Accept: 'application/json' });
+    }
+    const token = localStorage.getItem('altermap-token');
+    options.headers.set('Authorization', `Bearer ${token}`);
+    return fetchUtils.fetchJson(url, options);
+  };
+  const dataProvider = jsonServerProvider('/api/v1', httpClient);
+
   return (
-    <Admin dataProvider={jsonServerProvider('/api/v1')}>
+    <Admin dataProvider={dataProvider}>
       <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} />
     </Admin>
   );
