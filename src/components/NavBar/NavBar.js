c7310aaabaa8d627f31bbae0f189a8eedd51d175
sergio Prieto <saprieto5@gmail.com>
2020-01-27 16:15:19 +0100

    Add Basic circle buttons

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './NavBar.css';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faInfo, faListUl } from '@fortawesome/free-solid-svg-icons';
+import { faMap, faInfo, faListUl } from '@fortawesome/free-solid-svg-icons';
 import PdfExport from '../PdfExport/PdfExport';
 
 function NavBar({ close, closeTable }) {
   return (
     <div className="NavBar">
+      <button type="button" className="NavBar__info">
+        <Icon icon={faMap} className="NavBar__icon" />
+      </button>
       <button type="button" className="NavBar__info" onClick={close}>
         <Icon icon={faInfo} className="NavBar__icon" />
       </button>
